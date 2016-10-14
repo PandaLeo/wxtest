97e12fb2a47a17ed9dd40922607f283712a9e1aa
Administrator <544641781>
2016-10-14 11:48:13 +0800

    微场景测试

--- js/wechatapi.js
+++ js/wechatapi.js
@@ -20,8 +20,9 @@ var rmid = GetQueryString('rmid');
 var depth = GetQueryString('depth');
 var lineid = GetQueryString('lineid');
 var path = GetQueryString('path');
+var back_url = window.location.href;
 $.ajax({
-    url:"php/mz_view.php?mz_id="+id+'&rmid='+rmid,
+    url:"../../mz_view.php?mz_id="+id+'&rmid='+rmid,
     type:'post',
     data:{h5_url:h5_url},
     cache:false,
@@ -37,41 +38,42 @@ $.ajax({
         user_id = data.user_id;
     },
     error : function() {
-        window.location.href='php/get_allow.php';
+        window.location.href='../../get_allow.php?response_type=code&scope=snsapi_userinfo&state=snsapi_userinfo&back_url='+back_url;
     }
 });
 if(rmid==user_id){
     rmid='';
 }
-$.get('php/user_record.php?module=mz&id='+id+'&action=view&rmid='+rmid+'&depth='+depth+'&lineid='+lineid+'&path='+path);
-
-wx.config({
-    debug: false,
-    appId: appid,
-    timestamp: timestamp,
-    nonceStr: 'GZOgQmEI9hKgyCLg',
-    signature: signature,
-    jsApiList: [
-        'checkJsApi',
-        'onMenuShareTimeline',
-        'onMenuShareAppMessage',
-        'onMenuShareQQ',
-        'onMenuShareWeibo'
-    ]
-});
-wx.ready(function () {
-    var shareData = {
-        title: '自己填写',
-        desc: '自己填写',
-        link: out_url,
-        imgUrl: 'http://zmt.rychgf.com/test/h5/img/222.png',
-        success: function (){$.get(repeat_url);}
-    };
-    wx.onMenuShareAppMessage(shareData);
-    wx.onMenuShareTimeline(shareData);
-    wx.onMenuShareQQ(shareData);
-    wx.onMenuShareWeibo(shareData);
-});
+if(user_id){
+    $.get('../../user_record.php?module=mz&id='+id+'&action=view&rmid='+rmid+'&depth='+depth+'&lineid='+lineid+'&path='+path);
+    wx.config({
+        debug: false,
+        appId: appid,
+        timestamp: timestamp,
+        nonceStr: 'GZOgQmEI9hKgyCLg',
+        signature: signature,
+        jsApiList: [
+            'checkJsApi',
+            'onMenuShareTimeline',
+            'onMenuShareAppMessage',
+            'onMenuShareQQ',
+            'onMenuShareWeibo'
+        ]
+    });
+    wx.ready(function () {
+        var shareData = {
+            title: '自己填写',
+            desc: '自己填写',
+            link: out_url,
+            imgUrl: 'http://zmt.rychgf.com/test/h5/img/222.png',
+            success: function (){$.get(repeat_url);}
+        };
+        wx.onMenuShareAppMessage(shareData);
+        wx.onMenuShareTimeline(shareData);
+        wx.onMenuShareQQ(shareData);
+        wx.onMenuShareWeibo(shareData);
+    });
+}
 //微信去掉下方刷新栏
 if(navigator.userAgent.indexOf('MicroMessenger') >= 0){
     document.addEventListener('WeixinJSBridgeReady', function() {
